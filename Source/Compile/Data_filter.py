5766bf77cba78be8f93377f06f3236481931eb14
De1aY <6de1ay@gmail.com>
2017-01-25 09:51:28 -0500

    Fix: paths

--- Source/Compile/Data_filter.py
+++ Source/Compile/Data_filter.py
@@ -1,18 +1,4 @@
-
-# coding: utf-8
-
-# In[11]:
-
 import pandas as pd
-
-
-# In[12]:
-
-Sentences = pd.DataFrame.from_csv("../Data/sentences.csv")
-Sentences = Sentences.reset_index()
-
-
-# In[63]:
 
 def DeletePunctuation(Sentence):
     Sentence = Sentence.replace(",", " ")
@@ -36,9 +22,6 @@ def DeletePunctuation(Sentence):
     Sentence = Sentence.replace("...", " ")
     return Sentence
 
-
-# In[77]:
-
 def DeleteArticles(Array):
     Articles = ["без", "безо", "аль", "бы", "же", "но", "не", "то", "так", "а", "ах","близ", "в", "во", "вместо", "вне", "для", "до", "за", "из", "изо", "из-за", "из-под", "к", "ко", "кроме", "между", "меж", "на", "над", "надо", "о", "об", "обо", "от", "ото", "перед", "передо", "пo", "под", "при", "про", "ради", "с", "со", "сквозь", "среди", "у", "через", "чрез", "что", "и"]
     for aritcle in Articles:
@@ -48,9 +31,6 @@ def DeleteArticles(Array):
                 Result.append(element)
     return Result
 
-
-# In[78]:
-
 def DeleteNumbers(Array):
     Result = []
     for element in Array:
@@ -59,8 +39,6 @@ def DeleteNumbers(Array):
     return Result
 
 
-# In[79]:
-
 def CountArticles(Sentence):
     Result = {"!": 0, "?": 0, ".":0}
     Result["!"] = Sentence.count("!")
@@ -68,8 +46,6 @@ def CountArticles(Sentence):
     Result["?"] = Sentence.count("?")
     return Result
 
-
-# In[82]:
 
 def ParseSentence(Sentence):
     Sentence = Sentence.lower()
@@ -80,14 +56,5 @@ def ParseSentence(Sentence):
     Sentence = DeleteNumbers(Sentence)
     Sentence.append(Articles)
     return Sentence
-
-
-# In[81]:
-
-Sentences["Предложение"].apply(ParseSentence)
-
-
-# In[ ]:
-
 
 
