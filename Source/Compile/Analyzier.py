8aca5d2762efc351c601c8432b404366dee8aa18
De1aY <6de1ay@gmail.com>
2017-02-15 13:16:20 -0500

    Mark: 4.79

--- Source/Compile/Analyzier.py
+++ Source/Compile/Analyzier.py
@@ -5,6 +5,7 @@ words_df.columns = ['word', 'style']
 words_df = words_df.groupby('word')['style'].sum().to_frame().reset_index()
 words = words_df['word'].values
 words_style = words_df['style'].values
+Bigrams = pd.DataFrame.from_csv("../../Data/bigrams.csv").set_index('index').to_dict()
  
 def AnalyzeWords(Array):
     Count = 1
@@ -16,5 +17,39 @@ def AnalyzeWords(Array):
                 Count+=1
     return Sum / Count
 
+def AnalyzeNeutralBigrams(Array):
+    Coeff = 0
+    Array = list(set(Array))
+    for num in range(len(Array)):
+        for i in range(num+1, len(Array)):
+            if Array[num] != Array[i]:
+                try:
+                    Coeff += Bigrams['neutral'][Array[num]+"#"+Array[i]]
+                except:
+                    continue
+    return Coeff
 
+def AnalyzePositiveBigrams(Array):
+    Coeff = 0
+    Array = list(set(Array))
+    for num in range(len(Array)):
+        for i in range(num+1, len(Array)):
+            if Array[num] != Array[i]:
+                try:
+                    Coeff += Bigrams['positive'][Array[num]+"#"+Array[i]]
+                except:
+                    continue
+    return Coeff
+
+def AnalyzeNegativeBigrams(Array):
+    Coeff = 0
+    Array = list(set(Array))
+    for num in range(len(Array)):
+        for i in range(num+1, len(Array)):
+            if Array[num] != Array[i]:
+                try:
+                    Coeff += Bigrams['negative'][Array[num]+"#"+Array[i]]
+                except:
+                    continue
+    return Coeff
 
